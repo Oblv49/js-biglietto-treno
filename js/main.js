848a64342eeb30b435b3e77249147fd391430e34
DavideMinutoli <noprivacyworld@gmail.com>
2023-07-03 19:45:51 +0200

    imput form ok

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//input cliente per nome e cognome, km da percorrere e celta fascia de'età
+function submitForm(event){
+    
+    event.preventDefault();
+
+    //controllo se l'evento funziona
+    console.log("Il Form Funziona");
+
+    //variabili contenitori imput
+    let clientName = document.getElementById("nameAndSurname").value;
+    let kmToTravel = document.getElementById("km").value;
+    let age = document.getElementById("ageRange").checked;
+
+    //operazioni
+    
+
+}
