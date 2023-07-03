2ad49adacb2b5486346a97bd68eeea5da2801630
DavideMinutoli <noprivacyworld@gmail.com>
2023-07-03 19:59:57 +0200

    random codeCP and Carriage

--- js/main.js
+++ js/main.js
@@ -8,10 +8,13 @@ function submitForm(event){
 
     //variabili contenitori imput
     let clientName = document.getElementById("nameAndSurname").value;
-    let kmToTravel = document.getElementById("km").value;
+    let kmToTravel = parseInt(document.getElementById("km").value);
     let age = document.getElementById("ageRange").checked;
 
     //operazioni
-    
+    let ticketCost = kmToTravel * 0.21;
+    let carriageNumber = Math.floor(Math.random() * 7) + 1; 
+    let codeCp = Math.floor(Math.random() * 10000) + 1; 
+   
 
 }
