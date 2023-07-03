71e2307ea3cab7b2a070a7a93499cf8091d1879d
DavideMinutoli <noprivacyworld@gmail.com>
2023-07-03 20:27:43 +0200

    finish + bonus

--- js/main.js
+++ js/main.js
@@ -9,12 +9,40 @@ function submitForm(event){
     //variabili contenitori imput
     let clientName = document.getElementById("nameAndSurname").value;
     let kmToTravel = parseInt(document.getElementById("km").value);
-    let age = document.getElementById("ageRange").checked;
+    let age = document.getElementById("ageRange").value;
 
     //operazioni
     let ticketCost = kmToTravel * 0.21;
     let carriageNumber = Math.floor(Math.random() * 7) + 1; 
     let codeCp = Math.floor(Math.random() * 10000) + 1; 
-   
 
+    //calcolo prezzo in base alla scelta dell'utente
+    if (age == "20% sconto Minorenni"){
+        
+        document.getElementById("ticket").classList.remove("d-none");
+        document.getElementById("passengerName").innerHTML = clientName;
+        document.getElementById("typeOffer").innerHTML = age;
+        document.getElementById("carriage").innerHTML = carriageNumber;
+        document.getElementById("cpCode").innerHTML = codeCp;
+        document.getElementById("ticketPrice").innerHTML = ticketCost - ((ticketCost/100) * 20);
+    
+    } else if (age == "40% sconto Over65"){
+        
+        document.getElementById("ticket").classList.remove("d-none");
+        document.getElementById("passengerName").innerHTML = clientName;
+        document.getElementById("typeOffer").innerHTML = age;
+        document.getElementById("carriage").innerHTML = carriageNumber;
+        document.getElementById("cpCode").innerHTML = carriageNumber;
+        document.getElementById("ticketPrice").innerHTML = ticketCost - ((ticketCost/100) * 40);
+    
+    } else {
+
+        document.getElementById("ticket").classList.remove("d-none");
+        document.getElementById("passengerName").innerHTML = clientName;
+        document.getElementById("typeOffer").innerHTML = age;
+        document.getElementById("carriage").innerHTML = carriageNumber;
+        document.getElementById("cpCode").innerHTML = codeCp;
+        document.getElementById("ticketPrice").innerHTML = ticketCost;
+
+    }
 }
